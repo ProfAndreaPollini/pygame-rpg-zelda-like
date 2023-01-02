d35d56952eb4948a29607b34909f8f9d61cb2dc0
Andrea Pollini <prof.andrea.pollini@gmail.com>
2023-01-02 11:15:33 +0100

    creazione della classe Player(pg.sprite.Sprite) e rappresentazione a schermo

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ## Roadmap
 
-1. caricamento sprite per il player
-
+1. caricamento spritesheet per il player
+2. caricamento di un singolo sprite dallo spritesheet
+3. creazione della classe Player(pg.sprite.Sprite) e rappresentazione a schermo
+3. movimento dello sprite
 
 
 ## credits
--- main.py
+++ main.py
@@ -1,35 +1,52 @@
 import pygame as pg
 from settings import SCREEN_SIZE, SPRITE_SCALE
 
-print("hello world")
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        # load sprite
+        player_spritesheet = pg.image.load(
+            "assets/PixelMood4/Pixel Mood/Chars/Heros/Sprite sheet female hero.png").convert_alpha()
+
+        player_sprite = player_spritesheet.subsurface((0, 0, 16, 16))
+        original_size = player_sprite.get_size()
+
+        player_sprite = pg.transform.scale(
+            player_sprite, (original_size[0]*SPRITE_SCALE, original_size[1]*SPRITE_SCALE))
+
+        self.image : pg.surface.Surface = player_sprite
+        self.rect: pg.rect.Rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
 
 pg.init()
 screen = pg.display.set_mode(SCREEN_SIZE)
 clock = pg.time.Clock()
 
+player = Player(50, 50)
+
 running = True
 
-# load sprite
-player_spritesheet = pg.image.load(
-    "assets/PixelMood4/Pixel Mood/Chars/Heros/Sprite sheet female hero.png").convert_alpha()
-
-player_sprite = player_spritesheet.subsurface((0, 0, 16, 16))
-original_size = player_sprite.get_size()
-
-player_sprite = pg.transform.scale(
-    player_sprite, (original_size[0]*SPRITE_SCALE, original_size[1]*SPRITE_SCALE))
 
 while running:
-  for event in pg.event.get():
-    if event.type == pg.QUIT:
-      running = False
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
 
-  screen.fill("black")
+    screen.fill("black")
 
-  # show sprite
-  screen.blit(player_sprite, (50, 50))
+    
 
-  pg.display.flip()
+    # show sprite
+    screen.blit(player.image, player.rect)
+
+    pg.display.flip()
 
 
 pg.quit()
